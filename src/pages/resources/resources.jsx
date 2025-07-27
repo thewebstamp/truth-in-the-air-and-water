4586211a306a0b7774948e65283a9f779850f2e4
BAA77 <142021188+BAA77@users.noreply.github.com>
2025-07-27 09:30:43 -0700

    scroll

--- src/pages/resources/resources.jsx
+++ src/pages/resources/resources.jsx
@@ -1,14 +1,22 @@
 import Footer from "../../components/footer/footer.jsx";
 import Header from "../../components/header/header.jsx";
 import ResourcesComp from "../../components/resourcesComp/resourcesComp.jsx";
+import { Helmet } from 'react-helmet-async';
 
 function Resources() {
     return (
-        <div className="resources">
-            <Header resources='active' />
-            <ResourcesComp />
-            <Footer resources='active' />
-        </div>
+        <>
+            <Helmet>
+                <title>Resources | Verified Sources & Research | Truth in the Air and Water</title>
+                <meta name="description" content="Explore trusted sources, scientific studies, news articles, and investigative reports that support the information shared across our site. Everything you need to research chemtrails, water contamination, food toxins, and more — in one place." />
+            </Helmet>
+
+            <div className="resources">
+                <Header resources='active' />
+                <ResourcesComp />
+                <Footer resources='active' />
+            </div>
+        </>
     )
 };
 
